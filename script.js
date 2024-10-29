7a0c7738bc852681e4f6440d678289dd9213aa83
amanbhai1 <49727102+amanbhai1@users.noreply.github.com>
2024-10-30 02:19:07 +0530

    commit

--- script.js
+++ script.js
@@ -17,6 +17,12 @@ styleBtn.forEach((val, idx) => {
         neonText.classList.remove('rubik-scribble-regular')
         neonText.classList.remove('pacifico-regular')
         neonText.classList.remove('cursive')
+        neonText.classList.remove('Passionate')
+        neonText.classList.remove('Dope')
+        neonText.classList.remove('Play')
+        neonText.classList.remove('Challing')
+        neonText.classList.remove('Kallissa')
+        neonText.classList.remove('Better')
         if (val.innerHTML === 'Rubik') {
             neonText.classList.add('rubik-scribble-regular')
             console.log('if condi after 1')
@@ -26,6 +32,24 @@ styleBtn.forEach((val, idx) => {
         } else if (val.innerHTML === 'Cursive') {
             neonText.classList.add('cursive')
             console.log('if condi after 3')
+        }else if (val.innerHTML === 'Passionate') {
+            neonText.classList.add('Passionate')
+            console.log('if condi after 4')
+        }else if (val.innerHTML === 'Dope') {
+            neonText.classList.add('Dope')
+            console.log('if condi after 5')
+        }else if (val.innerHTML === 'Challing') {
+            neonText.classList.add('Challing')
+            console.log('if condi after 6')
+        }else if (val.innerHTML === 'Kallissa') {
+            neonText.classList.add('Kallissa')
+            console.log('if condi after 7')
+        }else if (val.innerHTML === 'Play') {
+            neonText.classList.add('Play')
+            console.log('if condi after 8')
+        }else if (val.innerHTML === 'Better') {
+            neonText.classList.add('Better')
+            console.log('if condi after 9')
         }
     })
 })
